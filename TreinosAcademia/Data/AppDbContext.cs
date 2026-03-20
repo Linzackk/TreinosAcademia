abc2aa3d47d791706fb715c950d3ec5a6fd40ca9
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-20 14:59:22 -0300

    Adição do Db Context e tabelas

---
+++ TreinosAcademia/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TreinosAcademia.Models;
+
+namespace TreinosAcademia.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Treino> Treinos { get; set; }
+        public DbSet<Exercicio> Exercicios { get; set; }
+        public DbSet<TreinoExercicio> TreinosExercicios { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+    }
+}
--- TreinosAcademia/Models/Usuario.cs
+++ TreinosAcademia/Models/Usuario.cs
@@ -4,6 +4,59 @@
     {
         public int Id { get; private set; }
         public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Senha { get; private set; }
 
+        private readonly List<Treino> _treinos = new List<Treino>();
+        public IReadOnlyCollection<Treino> Treinos => _treinos.AsReadOnly();
+
+        public Usuario(string nome, string email, string senha)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome não pode estar vazio.");
+
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentException("Email não pode estar vazio.");
+
+            if (string.IsNullOrEmpty(senha))
+                throw new ArgumentException("Senha não pode estar vazia.");
+
+            if (senha.Length <= 5)
+                throw new ArgumentException("Senha curta demais.");
+
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+        }
+
+        public void AlterarNome(string novoNome)
+        {
+            if (string.IsNullOrEmpty(novoNome))
+                throw new ArgumentException("Novo Nome não pode estar vazio.");
+
+            Nome = novoNome;
+        }
+
+        public void AlterarEmail(string novoEmail)
+        {
+            if (string.IsNullOrEmpty(novoEmail))
+                throw new ArgumentException("Novo E-mail não pode estar vazio.");
+
+            Email = novoEmail;
+        }
+
+        public void AlterarSenha(string novaSenha)
+        {
+            if (novaSenha == Senha)
+                throw new ArgumentException("A Nova senha deve ser diferente da anterior.");
+
+            if (string.IsNullOrEmpty(novaSenha))
+                throw new ArgumentException("Nova Senha não pode estar vazia.");
+
+            if (novaSenha.Length <= 5)
+                throw new ArgumentException("Senha curta demais.");
+
+            Senha = novaSenha;
+        }
     }
 }
