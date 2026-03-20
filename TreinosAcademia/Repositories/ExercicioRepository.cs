8ad946cf8f8bfbb308876a2a398dc7cc3bd42622
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-20 16:09:22 -0300

    feat ExercicioRepository métodos adicionados da Interface

---
+++ TreinosAcademia/Repositories/ExercicioRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TreinosAcademia.Data;
+using TreinosAcademia.Models;
+using TreinosAcademia.Repositories.Interfaces;
+
+namespace TreinosAcademia.Repositories
+{
+    public class ExercicioRepository : IExercicioRepository
+    {
+        private readonly AppDbContext _context;
+        public ExercicioRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Adicionar(Exercicio exercicio)
+        {
+            await _context.Exercicios.AddAsync(exercicio);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyCollection<Exercicio>> ObterTodos()
+        {
+            var exercicios = await _context.Exercicios.AsNoTracking().ToListAsync();
+            return exercicios;
+        }
+
+        public async Task<Exercicio> ObterPorId(int id)
+        {
+            var exercicio = await _context.Exercicios.FindAsync(id);
+            return exercicio;
+        }
+
+        public async Task Remover(Exercicio exercicio)
+        {
+            _context.Exercicios.Remove(exercicio);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Atualizar(Exercicio exercicio)
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
