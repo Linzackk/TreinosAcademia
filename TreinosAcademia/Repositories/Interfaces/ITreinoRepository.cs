c78c9f589ab098d53431f1b60f0e2652e6084877
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-20 16:36:22 -0300

    feat TreinoRepository para CRUD dos treinos

--- TreinosAcademia/Repositories/Interfaces/ITreinoRepository.cs
+++ TreinosAcademia/Repositories/Interfaces/ITreinoRepository.cs
@@ -7,6 +7,8 @@ namespace TreinosAcademia.Repositories.Interfaces
         Task Adicionar(Treino treino);
         Task<Treino> ObterPorId(int id);
         Task<IReadOnlyCollection<Treino>> ObterTodos();
+
+        Task<IReadOnlyCollection<Treino>> ObterTodosPorUsuario(int usuarioId);
         Task Atualizar(Treino treino);
         Task Remover(Treino treino);
     }
