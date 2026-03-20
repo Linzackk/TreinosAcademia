c34da28ac9a2a21a9531e33c06b909466febcf27
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-20 20:02:19 -0300

    feat configurações para Dependency Injections

--- TreinosAcademia/Program.cs
+++ TreinosAcademia/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using TreinosAcademia.Data;
+using TreinosAcademia.Repositories;
+using TreinosAcademia.Repositories.Interfaces;
+using TreinosAcademia.Services;
+using TreinosAcademia.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,18 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<ITreinoRepository, TreinoRepository>();
+builder.Services.AddScoped<ITreinoExercicioRepository, TreinoExercicioRepository>();
+builder.Services.AddScoped<IExercicioRepository, ExercicioRepository>();
+
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<ITreinoService, TreinoService>();
+builder.Services.AddScoped<ITreinoExercicioService, TreinoExercicioService>();
+builder.Services.AddScoped<IExercicioService, ExercicioService>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
