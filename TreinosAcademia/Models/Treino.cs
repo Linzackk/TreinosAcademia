a3872c304a3839c6815ae6a7eb93777c5bff09e6
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-20 14:56:41 -0300

    Criação dos Modelos do Projeto

---
+++ TreinosAcademia/Models/Treino.cs
@@ -0,0 +1,32 @@
+﻿namespace TreinosAcademia.Models
+{
+    public class Treino
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public int UsuarioId { get; private set; }
+        public Usuario Usuario { get; private set; }
+
+        private List<TreinoExercicio> _treinoExercicio = new List<TreinoExercicio>();
+        public IReadOnlyCollection<TreinoExercicio> TreinoExercicio => _treinoExercicio.AsReadOnly();
+
+        public Treino(string nome, int usuarioId)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome do Treino não pode estar vazio.");
+
+            if (usuarioId <= 0)
+                throw new ArgumentException("ID do usuario deve ser maior que 0.");
+            Nome = nome;
+            UsuarioId = usuarioId;
+        }
+
+        public void AlterarNome(string novoNome)
+        {
+            if (string.IsNullOrEmpty(novoNome))
+                throw new ArgumentException("Novo nome do Treino não pode estar vazio.");
+
+            Nome = novoNome;
+        }
+    }
+}
---
+++ TreinosAcademia/Models/TreinoExercicio.cs
@@ -0,0 +1,49 @@
+﻿namespace TreinosAcademia.Models
+{
+    public class TreinoExercicio
+    {
+        public int Id { get; private set; }
+        public int ExercicioId { get; private set; }
+        public Exercicio Exercicio { get; private set; }
+        public int TreinoId { get; private set; }
+        public Treino Treino { get; private set; }
+        public int Series { get; private set; }
+        public int Repeticoes { get; private set;  }
+
+        public TreinoExercicio(int exercicioId, int treinoId, int series, int repeticoes)
+        {
+            if (exercicioId <= 0)
+                throw new ArgumentException("ID do Exercício deve ser maior que 0.");
+
+            if (treinoId <= 0)
+                throw new ArgumentException("ID do Treino deve ser maior que 0.");
+
+            if (series <= 0)
+                throw new ArgumentException("Quantidade de séries deve ser maior que 0.");
+
+            if (repeticoes <= 0)
+                throw new ArgumentException("Quantidade de repetições deve ser maior que 0.");
+
+            ExercicioId = exercicioId;
+            TreinoId = treinoId;
+            Series = series;
+            Repeticoes = repeticoes;
+        }
+
+        public void AlterarQuantidadeSeries(int novaQuantidade)
+        {
+            if (novaQuantidade <= 0)
+                throw new ArgumentException("Quantidade de séries deve ser maior que 0.");
+
+            Series = novaQuantidade;
+        }
+
+        public void AlterarQuantidadeRepeticoes(int novaQuantidade)
+        {
+            if (novaQuantidade <= 0)
+                throw new ArgumentException("Quantidade de repetições deve ser maior que 0.");
+
+            Repeticoes = novaQuantidade;
+        }
+    }
+}
