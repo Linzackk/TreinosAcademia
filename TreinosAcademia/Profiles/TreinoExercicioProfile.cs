502b1192cb65fe263129538a856336501cac46f0
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-20 17:53:22 -0300

    feat Criação dos AutoMappers no projeto

---
+++ TreinosAcademia/Profiles/TreinoExercicioProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TreinosAcademia.DTOs.TreinoExercicio;
+using TreinosAcademia.Models;
+
+namespace TreinosAcademia.Profiles
+{
+    public class TreinoExercicioProfile : Profile
+    {
+        public TreinoExercicioProfile()
+        {
+            CreateMap<TreinoExercicio, TreinoExercicioResponseDTO>();
+            CreateMap<TreinoExercicioCreateDTO, TreinoExercicio>();
+            CreateMap<TreinoExercicioUpdateDTO, TreinoExercicio>();
+        }
+    }
+}
---
+++ TreinosAcademia/Profiles/TreinoProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using TreinosAcademia.DTOs.Treino;
+using TreinosAcademia.DTOs.TreinoExercicio;
+using TreinosAcademia.DTOs.Usuario;
+using TreinosAcademia.Models;
+
+namespace TreinosAcademia.Profiles
+{
+    public class TreinoProfile : Profile
+    {
+        public TreinoProfile()
+        {
+            CreateMap<TreinoExercicio, TreinoExercicioResponseDTO>()
+            .ForMember(dest => dest.ExercicioNome,
+                       opt => opt.MapFrom(src => src.Exercicio.Nome));
+            CreateMap<TreinoCreateDTO, Treino>();
+            CreateMap<TreinoUpdateDTO, Treino>();
+        }
+    }
+}
