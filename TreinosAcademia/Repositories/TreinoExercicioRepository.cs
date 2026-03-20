6364d2027c5f10c0c71a4d574cc9dcaf4e72400a
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-20 16:46:43 -0300

    feat Repositorio de TreinoExercicio

---
+++ TreinosAcademia/Repositories/TreinoExercicioRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TreinosAcademia.Data;
+using TreinosAcademia.Models;
+using TreinosAcademia.Repositories.Interfaces;
+
+namespace TreinosAcademia.Repositories
+{
+    public class TreinoExercicioRepository : ITreinoExercicioRepository
+    {
+        private readonly AppDbContext _context;
+        public TreinoExercicioRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Adicionar(TreinoExercicio treinoExercicio)
+        {
+            await _context.TreinosExercicios.AddAsync(treinoExercicio);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyCollection<TreinoExercicio>> ObterPorTreinoId(int treinoId)
+        {
+            var treinoExercicios = await _context.TreinosExercicios
+                .AsNoTracking()
+                .Include(te => te.Exercicio)
+                .Where(te => te.TreinoId == treinoId)
+                .ToListAsync();
+            return treinoExercicios;
+        }
+
+        public async Task Remover(TreinoExercicio treinoExercicio)
+        {
+            _context.TreinosExercicios.Remove(treinoExercicio);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Atualizar(TreinoExercicio treinoExercicio)
+        {
+            _context.TreinosExercicios.Update(treinoExercicio);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
